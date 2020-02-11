afd638db378cace86d8352928b443cfa52f1076a
Puskar Adhikari <puskar.ad@gmail.com>
2020-02-11 19:16:13 +0545

    Initial commit

--- app/data/contact.js
+++ app/data/contact.js
@@ -1,47 +1,42 @@
-import faGithub from '@fortawesome/fontawesome-free-brands/faGithub';
-import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebookF';
-import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram';
-import faLinkedinIn from '@fortawesome/fontawesome-free-brands/faLinkedinIn';
-import faAngellist from '@fortawesome/fontawesome-free-brands/faAngellist';
-import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter';
-import faEnvelope from '@fortawesome/fontawesome-free-regular/faEnvelope';
+import faGithub from "@fortawesome/fontawesome-free-brands/faGithub";
+import faFacebook from "@fortawesome/fontawesome-free-brands/faFacebookF";
+import faInstagram from "@fortawesome/fontawesome-free-brands/faInstagram";
+import faLinkedinIn from "@fortawesome/fontawesome-free-brands/faLinkedinIn";
+import faAngellist from "@fortawesome/fontawesome-free-brands/faAngellist";
+import faTwitter from "@fortawesome/fontawesome-free-brands/faTwitter";
+import faEnvelope from "@fortawesome/fontawesome-free-regular/faEnvelope";
 
 const data = [
   {
-    link: 'https://github.com/mldangelo',
-    label: 'Github',
-    icon: faGithub,
+    link: "https://github.com/devpuskar",
+    label: "Github",
+    icon: faGithub
   },
   {
-    link: 'https://facebook.com/d',
-    label: 'Facebook',
-    icon: faFacebook,
+    link: "https://www.facebook.com/puskar.ad12",
+    label: "Facebook",
+    icon: faFacebook
   },
   {
-    link: 'https://www.instagram.com/dangelosaurus/',
-    label: 'Instagram',
-    icon: faInstagram,
+    link: "https://www.instagram.com/dev_puskar/",
+    label: "Instagram",
+    icon: faInstagram
   },
   {
-    link: 'https://www.linkedin.com/in/michaelldangelo',
-    label: 'LinkedIn',
-    icon: faLinkedinIn,
+    link: "https://www.linkedin.com/in/devpuskar/",
+    label: "LinkedIn",
+    icon: faLinkedinIn
   },
   {
-    link: 'https://angel.co/michael-d-angelo',
-    label: 'Angel List',
-    icon: faAngellist,
+    link: "https://twitter.com/dev_puskar_",
+    label: "Twitter",
+    icon: faTwitter
   },
   {
-    link: 'https://twitter.com/dangelosaurus',
-    label: 'Twitter',
-    icon: faTwitter,
-  },
-  {
-    link: 'mailto:michael.l.dangelo@gmail.com',
-    label: 'Email',
-    icon: faEnvelope,
-  },
+    link: "mailto:puskar.ad@gmail.com",
+    label: "Email",
+    icon: faEnvelope
+  }
 ];
 
 export default data;
